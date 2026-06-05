837c81947875b6966bb5fe77f04f027eca727322
Vergel1231 <69107513+Vergel1231@users.noreply.github.com>
2026-06-05 21:53:58 +0800

    Update api.js

--- frontend/src/lib/api.js
+++ frontend/src/lib/api.js
@@ -9,6 +9,14 @@ const api = axios.create({
   headers: { "Content-Type": "application/json" },
 });
 
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem("access_token");
+  if (token) {
+    config.headers["Authorization"] = `Bearer ${token}`;
+  }
+  return config;
+});
+
 export default api;
 
 export function formatApiError(err) {
